cb102d4053317b7fc3ad0f0ea645c43e8dc8190c
Stephen P <123outerme@outlook.com>
2018-01-27 17:48:12 -0500

    Update outerBot.js

--- js/outerBot.js
+++ js/outerBot.js
@@ -35,6 +35,6 @@ outerBot.executeCommand =  function(data) {
   if (message.startsWith("sarcasm "))
     outerBot.respond(poster + " says \"" + rawMessage.substring(9) + "\" sarcastically.");
     
-  if (message.startsWith("echo "))
+  if (message.startsWith("echo ") && message.substring(5,6) != "/")
     outerBot.respond(rawMessage.substring(6));
 }
